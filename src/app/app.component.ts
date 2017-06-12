96b2f84edaaad377d536d87f2fdc4531f3d101e2
davidredondo52 <davidredondo52@hotmail.com>
2017-06-12 13:40:01 +0200

    ultimos retoques

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,23 @@ import { Marcador } from './interfaces/marcador.interface';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+
+ 
   lat: number = 40.417130;
   lng: number = -3.706539;
   zoom:number=16;
 
+  marcadorSel:any=null;
+  draggable:string="1";
+
   constructor(private _mapService:MapasService)
   {
-
+    this._mapService.cargarMarcadores();
   }
 
-  clickMapa(evento){
+  clickMapa(evento)
+  {
     let nuevoMarcador:Marcador={
       lat:evento.coords.lat,
       lng:evento.coords.lng,
@@ -26,5 +32,52 @@ export class AppComponent {
     }
   	console.log(evento);
     this._mapService.nuevoMarcador(nuevoMarcador);
+
   }
+
+  clickMarcador(marcador:Marcador,i:number)
+  {
+      this.marcadorSel=marcador;
+    
+      if(this.marcadorSel.draggable)
+      {
+        this.draggable="1"
+      }
+      else
+      {
+        this.draggable="0"
+      }
+    }
+
+  dragEndMarcador(marcador:Marcador,evento)
+   {
+     console.log(marcador);
+     console.log(evento);
+     let lat=evento.coords.lat;
+     let lng=evento.coords.lng;
+     marcador.lat=lat;
+     marcador.lng=lng;
+     this._mapService.guardarMarcadores();
+   }
+
+   borrarMarcador(i:number)
+   {
+     
+     this._mapService.borrarMarcador(i);
+     this.marcadorSel=null;
+   }
+
+   cambiarDraggable()
+   {
+     if(this.draggable=="1")
+     {
+       this.marcadorSel.draggable=true;
+     }
+     else
+     {
+      this.marcadorSel.draggable=false;
+     }
+   
+   }
 }
+
--- src/app/services/mapas.service.ts
+++ src/app/services/mapas.service.ts
@@ -15,6 +15,31 @@ export class MapasService {
 
   }
    nuevoMarcador(marcador:Marcador){
-   	this.marcadores.push(marcador);
+   	 this.marcadores.push(marcador);
+     this.guardarMarcadores();
    }
-}
+
+   guardarMarcadores()
+   {
+     localStorage.setItem('marcadores',JSON.stringify(this.marcadores));
+   }
+   cargarMarcadores()
+   {
+     if(localStorage.getItem('marcadores')){
+       this.marcadores=JSON.parse(localStorage.getItem('marcadores'));
+     }
+     else{
+       this.marcadores=[];
+     }
+   }
+
+   borrarMarcador(i:number)
+   {
+     
+     this.marcadores.splice(i,1);
+    
+      this.guardarMarcadores();
+   }
+ }
+
+
